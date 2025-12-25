64a52e4d7c8ff370195a006cb539294a81e50146
RaZeRXNight <lazerx38@gmail.com>
2025-12-25 11:59:41 -0500

    Added Minor Error handling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from sys import argv
+from http.client import HTTPResponse
 from urllib.request import *
+import json;
 
 # https://api.github.com/users/<username>/events
 # Example: https://api.github.com/users/kamranahmedse/events
@@ -11,14 +13,24 @@ def main():
         raise Exception("ERROR: Improper usage, Example: python3 gh-activity.py <username>")
     
     user = argv[-1]
-    url = f"https://api.github.com/users/{user}"
+    url = f"https://api.github.com/users/{user}/events"
     
+    try:
+        with urlopen(url) as api:
+            if not isinstance(api, HTTPResponse):
+                raise Exception(f"ERROR: HTTP RESPONSE NOT RETURNED")
+            
+            if api.status != 200:
+                raise Exception(f"ERROR: {api.status}")
+            
+            header = api.headers
+            data = api.read()
+            if "application/json" not in api.headers["Content-Type"]:
+                decoded_json = json.loads(data)
+                print(decoded_json)
+    except Exception as e:
+        print(e)
 
-    api_request = urlopen(url, None, 5.0)
-    
-    if api_request.status != 200:
-        raise Exception(f"ERROR: {api_request.status}")
-    
     
     
 if __name__ == "__main__":
